e94ac074c122fe7142ab3a391f0fa72541962e1e
Nabil <nabilaltamash@gmail.com>
2022-08-10 21:24:48 +0600

    DO THIS

--- src/Component/Login/SignIn.jsx
+++ src/Component/Login/SignIn.jsx
@@ -1,31 +1,19 @@
-import React,{useState} from 'react';
+import React from 'react';
 import '../Login/signin.css';
 
-const SignIn= (props)=>{
-
-    const [userInfo, setUserInfo] = useState({
-        email:'',
-        password: '',
-        confirmPass: '',
-    });
-
-    const inputHandler = (e)=>{
-        const value = e.target.value;
-    
-    }
-    return(
-        <>
-            <div className='signin_input'>
-                <input 
-                onChange={inputHandler}
-                type={props.type} 
-                autoComplete = {props.autoComplete} 
-                name = {props.name} 
-                placeholder= {props.placeholder}
-                />
-            </div>
-        </>
-    );
-}
+const SignIn = ({ state, setState, type, autoComplete, name, placeholder }) => {
+  return (
+    <div className='signin_input'>
+      <input
+        value={state}
+        onChange={e => setState(e.target.value)}
+        type={type}
+        autoComplete={autoComplete}
+        name={name}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
 
 export default SignIn;
